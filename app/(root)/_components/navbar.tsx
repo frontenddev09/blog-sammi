3872a9ed326633f40b132716fb0cf5a8e72c611a
ozodbekqosimov <o0319353@gmail.com>
2025-05-12 15:27:20 +0500

    updated

--- app/(root)/_components/navbar.tsx
+++ app/(root)/_components/navbar.tsx
@@ -15,7 +15,7 @@ function Navbar() {
 			<div className='container max-w-6xl mx-auto h-[10vh] w-full flex items-center justify-between'>
 				{/* Logo */}
 				<Link href={'/'}>
-					<h1 className='text-4xl font-creteRound'>Sammi</h1>
+					<h1 className='text-4xl font-creteRound'>DevBlog</h1>
 				</Link>
 				{/* Nav links */}
 				<div className='gap-2 hidden md:flex'>
